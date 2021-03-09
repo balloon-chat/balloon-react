b619d73a16af5ca026ba87a054cc7aa842ae7cbb
masaki <robomasaki2@gmail.com>
2021-03-09 09:45:27 +0900

    feat(room domain): added description model

--- src/data/core/room/roomDto.ts
+++ src/data/core/room/roomDto.ts
@@ -2,31 +2,75 @@ import { Room } from 'src/domain/room/models/room';
 import { RoomId } from 'src/domain/room/models/roomId';
 import { RoomTitle } from 'src/domain/room/models/roomTitle';
 import { UserId } from 'src/domain/user/models/userId';
+import { RoomDescription } from 'src/domain/room/models/roomDescription';
+import { RoomEntity } from 'src/domain/room/repository/roomEntity';
 
 export class RoomDto {
   constructor(
       readonly id: string,
       readonly title: string,
+      readonly description: string,
       readonly createdAt: number,
       readonly createdBy: string,
   ) {
   }
 
-  static from(room: Room): RoomDto {
+  static from(room: RoomEntity): RoomDto {
     return new RoomDto(
         room.id.value,
         room.title.value,
+        room.description?.value ?? '',
         room.createdAt,
         room.createdBy.value,
     );
   }
 
-  toRoom(): Room {
+  static fromJSON(json: Object | null): RoomDto | undefined {
+    if (json && isRoomJSON(json)) {
+      const src = json as RoomJSON;
+      return new RoomDto(
+          src.id,
+          src.title,
+          src.description,
+          src.createdAt,
+          src.createdBy,
+      );
+    }
+  }
+
+  toRoomEntity(): RoomEntity {
     return new Room(
         new RoomId(this.id),
         new RoomTitle(this.title),
         this.createdAt,
         new UserId(this.createdBy),
+        RoomDescription.create(this.description),
     );
   }
+
+  toJSON(): RoomJSON {
+    return {
+      id: this.id,
+      title: this.title,
+      description: this.description,
+      createdAt: this.createdAt,
+      createdBy: this.createdBy,
+    };
+  }
 }
+
+type RoomJSON = {
+  id: string,
+  title: string,
+  description: string,
+  createdAt: number,
+  createdBy: string,
+};
+
+const isRoomJSON = (obj: any): obj is RoomJSON => {
+  return typeof obj.id === 'string'
+      && typeof obj.title === 'string'
+      && typeof obj.description === 'string'
+      && typeof obj.createdAt === 'number'
+      && typeof obj.createdBy === 'string';
+};
--- src/domain/room/models/room.ts
+++ src/domain/room/models/room.ts
@@ -1,6 +1,7 @@
 import { RoomId } from 'src/domain/room/models/roomId';
 import { RoomTitle } from 'src/domain/room/models/roomTitle';
 import { UserId } from 'src/domain/user/models/userId';
+import { RoomDescription } from 'src/domain/room/models/roomDescription';
 
 export class Room {
   constructor(
@@ -8,17 +9,19 @@ export class Room {
       public readonly title: RoomTitle,
       public readonly createdAt: number,
       public readonly createdBy: UserId,
+      public readonly description?: RoomDescription,
   ) {
   }
 }
 
 export class RoomFactory {
-  create(title: RoomTitle, createdBy: UserId): Room {
+  create(title: RoomTitle, createdBy: UserId, description?: string): Room {
     return new Room(
         new RoomId(),
         title,
         Date.now(),
         createdBy,
+        RoomDescription.create(description),
     );
   }
 }
