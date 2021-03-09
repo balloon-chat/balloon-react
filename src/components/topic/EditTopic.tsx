e17522dd46323d5656b6467bcd476f0c6d1f6446
masaki <robomasaki2@gmail.com>
2021-03-09 12:27:51 +0900

    feat(room): create new topic
    ページから話題(Room)を作成し、
    作成されたRoomに移動できるようにした。

--- src/components/topic/EditTopic.tsx
+++ src/components/topic/EditTopic.tsx
@@ -1,23 +1,58 @@
-import { ContainerCard } from 'src/components/topic/ContainerCard';
-import { CSSProperties } from 'react';
+import { CSSProperties, FormEvent, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Button } from 'src/components/topic/Button';
+import { useDispatch } from 'react-redux';
+import { useUserSelector } from 'src/data/redux/user/selector';
+import { createRoom } from 'src/data/redux/room/action';
+import { useRoomState } from 'src/data/redux/room/selector';
+import { useRouter } from 'next/router';
+import { setIsRoomCreated } from 'src/data/redux/room/slice';
+import { UserService } from 'src/domain/user/service/userService';
+import { setUserId } from 'src/data/redux/user/slice';
 
 // tslint:disable-next-line:variable-name
 export const EditTopic = () => {
-  return (<ContainerCard>
-    <form style={form}>
-      <label style={field}>
-        <div style={title}>タイトル</div>
-        <Input/>
-      </label>
-      <label style={field}>
-        <div style={title}>簡単な説明</div>
-        <Input/>
-      </label>
-      <Button style={createButton}>作成</Button>
-    </form>
-  </ContainerCard>);
+
+  const dispatcher = useDispatch();
+  const router = useRouter();
+  const userId = useUserSelector().uid;
+  const roomId = useRoomState().roomId;
+  const isRoomCreated = useRoomState().isRoomCreated;
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  // TODO: remove this
+  useEffect(() => {
+    const service = new UserService();
+    dispatcher(setUserId(service.getCurrentUserId().value));
+  },        []);
+
+  useEffect(() => {
+    if (roomId && isRoomCreated) navigateToRoom(roomId).then();
+  },        [roomId, isRoomCreated]);
+
+  const navigateToRoom = async (roomId: string) => {
+    dispatcher(setIsRoomCreated({ isRoomCreated: true }));
+    await router.push(`/topics/${roomId}`);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (title && userId) dispatcher(createRoom({ title, userId, description }));
+  };
+
+  return (<form style={form} onSubmit={handleSubmit}>
+    <label style={field}>
+      <div style={titleStyle}>タイトル</div>
+      <Input onChange={e => setTitle(e.target.value)}/>
+    </label>
+    <label style={field}>
+      <div style={titleStyle}>簡単な説明</div>
+      <Input onChange={e => setDescription(e.target.value)}/>
+    </label>
+    <Button style={createButton}>作成</Button>
+  </form>);
 };
 
 const form: CSSProperties = {
@@ -34,7 +69,7 @@ const field: CSSProperties = {
   marginBottom: 24,
 } as const;
 
-const title: CSSProperties = {
+const titleStyle: CSSProperties = {
   color: 'rgba(0, 0, 0, .6)',
   fontSize: 14,
   marginBottom: 4,
--- src/pages/topics/create.tsx
+++ src/pages/topics/create.tsx
@@ -1,13 +1,16 @@
 import { NavBarSmall } from 'src/components/navbar/NavBar';
 import { EditTopic } from 'src/components/topic/EditTopic';
 import React, { CSSProperties } from 'react';
+import { ContainerCard } from 'src/components/topic/ContainerCard';
 
 // tslint:disable-next-line:variable-name
 const CreateTopicPage = () => {
   return (<div>
     <NavBarSmall/>
     <div style={body}>
-      <EditTopic/>
+      <ContainerCard>
+        <EditTopic/>
+      </ContainerCard>
     </div>
   </div>);
 };
