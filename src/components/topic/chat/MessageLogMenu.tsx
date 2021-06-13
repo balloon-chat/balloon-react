d68ccc999d6ac0fccf8a513c5ac5fe61b1c87170
masaki <robomasaki2@gmail.com>
2021-06-13 09:44:07 +0900

    feat: message log icon
    ログを表示するためのアイコンを、
    ハンバーガーアイコンから吹き出しのアイコンに変更した。

--- src/components/topic/chat/MessageLogMenu.tsx
+++ src/components/topic/chat/MessageLogMenu.tsx
@@ -16,7 +16,7 @@ export const MessageLogMenu = () => {
         onClick={useCallback(() => setIsVisible((prev) => !prev), [])}
         isVisible={isVisible}
       >
-        <MenuIcon />
+        <ChatBubble />
       </MenuIconContainer>
     </>
   );
